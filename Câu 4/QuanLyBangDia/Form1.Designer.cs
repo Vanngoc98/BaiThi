109ced6c2ea65a498fda6e1b1656c9990dfbb67c
Dark <56054516+Vanngoc98@users.noreply.github.com>
2021-03-03 06:56:11 +0700

    Add files via upload

---
+++ Câu 4/QuanLyBangDia/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace QuanLyBangDia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.TBMBD = new System.Windows.Forms.TextBox();
+            this.TBTBD = new System.Windows.Forms.TextBox();
+            this.TBSL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BTNhap = new System.Windows.Forms.Button();
+            this.BTSua = new System.Windows.Forms.Button();
+            this.BTXoa = new System.Windows.Forms.Button();
+            this.BTThoat = new System.Windows.Forms.Button();
+            this.DGRW = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGRW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TBSL);
+            this.groupBox1.Controls.Add(this.TBTBD);
+            this.groupBox1.Controls.Add(this.TBMBD);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(36, 51);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1261, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập Thông Tin Băng Đĩa";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.BTThoat);
+            this.groupBox2.Controls.Add(this.BTXoa);
+            this.groupBox2.Controls.Add(this.BTSua);
+            this.groupBox2.Controls.Add(this.BTNhap);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(36, 217);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(1261, 135);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.DGRW);
+            this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox3.Location = new System.Drawing.Point(36, 385);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(1261, 266);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Thông Tin Chi Tiết Băng Đĩa";
+            // 
+            // TBMBD
+            // 
+            this.TBMBD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBMBD.Location = new System.Drawing.Point(156, 55);
+            this.TBMBD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TBMBD.Name = "TBMBD";
+            this.TBMBD.Size = new System.Drawing.Size(203, 35);
+            this.TBMBD.TabIndex = 0;
+            // 
+            // TBTBD
+            // 
+            this.TBTBD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBTBD.Location = new System.Drawing.Point(565, 55);
+            this.TBTBD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TBTBD.Name = "TBTBD";
+            this.TBTBD.Size = new System.Drawing.Size(203, 35);
+            this.TBTBD.TabIndex = 1;
+            // 
+            // TBSL
+            // 
+            this.TBSL.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBSL.Location = new System.Drawing.Point(987, 55);
+            this.TBSL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TBSL.Name = "TBSL";
+            this.TBSL.Size = new System.Drawing.Size(203, 35);
+            this.TBSL.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(8, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mã Băng Đĩa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(412, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tên Băng Đĩa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(873, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Số Lượng";
+            // 
+            // BTNhap
+            // 
+            this.BTNhap.BackColor = System.Drawing.Color.Blue;
+            this.BTNhap.ForeColor = System.Drawing.Color.Red;
+            this.BTNhap.Location = new System.Drawing.Point(36, 52);
+            this.BTNhap.Name = "BTNhap";
+            this.BTNhap.Size = new System.Drawing.Size(98, 47);
+            this.BTNhap.TabIndex = 0;
+            this.BTNhap.Text = "Nhập";
+            this.BTNhap.UseVisualStyleBackColor = false;
+            this.BTNhap.Click += new System.EventHandler(this.BTNhap_Click);
+            // 
+            // BTSua
+            // 
+            this.BTSua.BackColor = System.Drawing.Color.Blue;
+            this.BTSua.ForeColor = System.Drawing.Color.Red;
+            this.BTSua.Location = new System.Drawing.Point(329, 52);
+            this.BTSua.Name = "BTSua";
+            this.BTSua.Size = new System.Drawing.Size(98, 47);
+            this.BTSua.TabIndex = 1;
+            this.BTSua.Text = "Sửa";
+            this.BTSua.UseVisualStyleBackColor = false;
+            this.BTSua.Click += new System.EventHandler(this.BTSua_Click);
+            // 
+            // BTXoa
+            // 
+            this.BTXoa.BackColor = System.Drawing.Color.Blue;
+            this.BTXoa.ForeColor = System.Drawing.Color.Red;
+            this.BTXoa.Location = new System.Drawing.Point(633, 52);
+            this.BTXoa.Name = "BTXoa";
+            this.BTXoa.Size = new System.Drawing.Size(98, 47);
+            this.BTXoa.TabIndex = 2;
+            this.BTXoa.Text = "Xóa";
+            this.BTXoa.UseVisualStyleBackColor = false;
+            this.BTXoa.Click += new System.EventHandler(this.BTXoa_Click);
+            // 
+            // BTThoat
+            // 
+            this.BTThoat.BackColor = System.Drawing.Color.Blue;
+            this.BTThoat.ForeColor = System.Drawing.Color.Red;
+            this.BTThoat.Location = new System.Drawing.Point(949, 52);
+            this.BTThoat.Name = "BTThoat";
+            this.BTThoat.Size = new System.Drawing.Size(98, 47);
+            this.BTThoat.TabIndex = 3;
+            this.BTThoat.Text = "Thoat";
+            this.BTThoat.UseVisualStyleBackColor = false;
+            this.BTThoat.Click += new System.EventHandler(this.BTThoat_Click);
+            // 
+            // DGRW
+            // 
+            this.DGRW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGRW.Location = new System.Drawing.Point(13, 56);
+            this.DGRW.Name = "DGRW";
+            this.DGRW.RowHeadersWidth = 62;
+            this.DGRW.RowTemplate.Height = 28;
+            this.DGRW.Size = new System.Drawing.Size(1241, 203);
+            this.DGRW.TabIndex = 0;
+            this.DGRW.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGRW_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 671);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Quản Lý Băng Đĩa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGRW)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBSL;
+        private System.Windows.Forms.TextBox TBTBD;
+        private System.Windows.Forms.TextBox TBMBD;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button BTThoat;
+        private System.Windows.Forms.Button BTXoa;
+        private System.Windows.Forms.Button BTSua;
+        private System.Windows.Forms.Button BTNhap;
+        private System.Windows.Forms.DataGridView DGRW;
+    }
+}
+
---
+++ Câu 4/QuanLyBangDia/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyBangDia
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string str = @"Data Source=DARK\DARK;Initial Catalog=QuanLyBangDia;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+        void loaddata()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select * from BangDia";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            DGRW.DataSource = table;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(str);
+            connection.Open();
+            loaddata();
+        }
+
+        private void DGRW_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            TBMBD.ReadOnly = true;
+            int i;
+            i = DGRW.CurrentRow.Index;
+            TBMBD.Text = DGRW.Rows[i].Cells[0].Value.ToString();
+            TBTBD.Text = DGRW.Rows[i].Cells[1].Value.ToString();
+            TBSL.Text = DGRW.Rows[i].Cells[2].Value.ToString();
+        }
+
+        private void BTNhap_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "insert into BangDia values('"+TBMBD.Text+ "',N'"+TBTBD.Text+"','" +TBSL.Text+"')";
+            command.ExecuteNonQuery();
+            loaddata();
+        }
+        private void BTSua_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "update BangDia set TenBangDia ='"+TBTBD.Text+"',SoLuong='"+TBSL.Text+"'where MaBD ='"+TBMBD.Text+"'";
+            command.ExecuteNonQuery();
+            loaddata();
+        }
+        private void BTXoa_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "delete from BangDia where MaBD ='"+TBMBD.Text +"'";
+            command.ExecuteNonQuery();
+            loaddata();
+        }
+
+     
+    }
+}
